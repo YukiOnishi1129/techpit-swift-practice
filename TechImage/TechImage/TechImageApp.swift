b77d8de78290f217fd82ad8333bd498dc859580f
yuki <yuki.onishi1129@gmail.com>
2021-10-05 17:45:50 +0900

    add: setup

---
+++ TechImage/TechImage/TechImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechImageApp.swift
+//  TechImage
+//
+//  Created by YUKI on 2021/10/05.
+//
+
+import SwiftUI
+
+@main
+struct TechImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
